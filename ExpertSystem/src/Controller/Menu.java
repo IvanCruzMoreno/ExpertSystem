364da8aa7ea0d9c2f3a07852754217b336b349a0
ivan <ivanmoreno405@gmail.com>
2021-05-15 16:35:37 -0500

    added first section

--- ExpertSystem/src/Controller/Menu.java
+++ ExpertSystem/src/Controller/Menu.java
@@ -2,12 +2,44 @@ package Controller;
 
 import java.util.Scanner;
 
+import Seccion1.Ventas;
+
 public class Menu {
 
+	
 	public static void main(String[] args) {
 		
+		Ventas ventas = new Ventas();
+		
+		System.out.println("------------------");
+		System.out.println("Bienvenido Experto");
+		System.out.println("------------------");
+		
+		Menu.showOptions();
+		
 		Scanner scanner = new Scanner(System.in);
-		String menu = scanner.nextLine();
+		int menu = scanner.nextInt();
+		
+		switch (menu) {
+		
+			case 1:
+				ventas.saleProcess();
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			default:
+				break;
+		}
+	}
+	
+	public static void showOptions() {
+		System.out.println("1) Realizar Venta");
+		System.out.println("2) Visualizacion de estantes");
+		System.out.println("3) Gastos Totales");
+		System.out.println("------------------\n");
+		System.out.print("Selecciona una opcion: ");
 	}
 
 }
--- ExpertSystem/src/Controller/Menu_REMOTE_1685.java
+++
@@ -1,8 +0,0 @@
-package Controller;
-
-public class Menu {
-	
-	public static void main (String args []) {
-		System.out.println("Estamos dentro");
-	}
-}
