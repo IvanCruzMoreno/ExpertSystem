baf131e8f69deecc0e528348f3854c47864c4627
ivan <ivanmoreno405@gmail.com>
2021-05-15 13:56:04 -0500

    merge files

---
+++ ExpertSystem/src/Controller/Menu_BACKUP_1685.java
@@ -0,0 +1,13 @@
+package Controller;
+
+import java.util.Scanner;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		String menu = scanner.nextLine();
+	}
+
+}
---
+++ ExpertSystem/src/Controller/Menu_REMOTE_1685.java
@@ -0,0 +1,8 @@
+package Controller;
+
+public class Menu {
+	
+	public static void main (String args []) {
+		System.out.println("Estamos dentro");
+	}
+}
