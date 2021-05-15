ce9f9c038301c13786e6fb0469ecd850ace8c71f
ivan <ivanmoreno405@gmail.com>
2021-05-15 13:37:42 -0500

    added menu

--- ExpertSystem/src/Controller/Menu.java
+++ ExpertSystem/src/Controller/Menu.java
@@ -1,6 +1,14 @@
 package Controller;
 
+import java.util.Scanner;
+
 public class Menu {
 
-	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		String menu = scanner.nextLine();
+		
+		
+	}
 }
