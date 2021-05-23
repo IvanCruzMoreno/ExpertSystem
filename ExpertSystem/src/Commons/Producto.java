ddda355ce8f2d75c29403cc4b0eae0e2fc0fc181
ivan <ivanmoreno405@gmail.com>
2021-05-23 15:24:59 -0500

    constructor and idProducto added

--- ExpertSystem/src/Commons/Producto.java
+++ ExpertSystem/src/Commons/Producto.java
@@ -2,11 +2,28 @@ package Commons;
 
 public class Producto {
 	
+	private int idProducto;
+	
 	private String nombre;
 	
 	private int cantidadVendidos;
 	
 	private double precio;
+
+	public Producto(int idProducto, int cantidadVendidos) {
+		this.idProducto = idProducto;
+		this.cantidadVendidos = cantidadVendidos;
+	}
+	public Producto() {
+		
+	}
+	public int getIdProducto() {
+		return idProducto;
+	}
+
+	public void setIdProducto(int idProducto) {
+		this.idProducto = idProducto;
+	}
 
 	/**
 	 * @return the nombre
@@ -49,4 +66,5 @@ public class Producto {
 	public void setPrecio(double precio) {
 		this.precio = precio;
 	}
+	
 }
